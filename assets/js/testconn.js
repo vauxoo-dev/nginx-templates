893e6d408a4c400bf65dc215d79330b7832fff93
Tulio Ruiz <tulio@vauxoo.com>
2014-12-02 11:11:29 -0600

    [IMP] Added autocheck every 10 secs and show stats

---
+++ assets/js/testconn.js
@@ -0,0 +1,28 @@
+
+function ping(url) {
+    var encodedURL = encodeURIComponent(url);
+    var startDate = new Date();
+    var endDate = null;
+    jQuery.ajax({
+      url: "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22" + encodedURL + "%22&format=json",
+      type: "get",
+      async: false,
+      dataType: "json",
+      success: function(data) {
+        if (data.query.results != null) {
+            endDate = new Date();
+        } else {
+            endDate = null;
+        }
+      },
+      error: function(){
+        endDate = null;
+      }
+    });
+
+    if (endDate == null) {
+        return null;
+    }
+
+    return endDate.getTime() - startDate.getTime();
+}
